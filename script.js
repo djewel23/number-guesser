dfe9c280c989df97f566cb2a52d53bca80ac74f6
Daniel J <68441865+djewel23@users.noreply.github.com>
2022-06-13 14:44:21 -0600

    Changed Prettier quote settings to single

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // console.log(document.querySelector('.message').textContent);
 
@@ -13,36 +13,36 @@ const ranNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 const guessFunc = function () {
-  const guess = Number(document.querySelector(".guess").value);
+  const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔️ No Number!";
-    console.log("Nothing here");
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+    console.log('Nothing here');
   } else if (guess === ranNumber) {
-    document.querySelector(".message").textContent = "🎊 Correct number!";
+    document.querySelector('.message').textContent = '🎊 Correct number!';
   } else if (guess > ranNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "📈 Guess too high!";
+      document.querySelector('.message').textContent = '📈 Guess too high!';
       score--;
-      document.querySelector(".score").textContent = score;
+      document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector(".message").textContent = "🫣 You loose!";
-      document.querySelector(".score").textContent = 0;
+      document.querySelector('.message').textContent = '🫣 You loose!';
+      document.querySelector('.score').textContent = 0;
     }
   } else if (guess < ranNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "📉 Guess too low!";
+      document.querySelector('.message').textContent = '📉 Guess too low!';
       score--;
-      document.querySelector(".score").textContent = score;
+      document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector(".message").textContent = "🫣 You loose!";
-      document.querySelector(".score").textContent = 0;
+      document.querySelector('.message').textContent = '🫣 You loose!';
+      document.querySelector('.score').textContent = 0;
     }
   }
 };
 
-document.querySelector(".check").addEventListener("click", guessFunc);
-document.querySelector(".number").textContent = ranNumber;
+document.querySelector('.check').addEventListener('click', guessFunc);
+document.querySelector('.number').textContent = ranNumber;
 
 // document.querySelector('.check').addEventListener('click', function() {
 //   console.log(document.querySelector('.guess').value);

0e35f627898f1d9ada712fe545d8adc1cd28f1bd
Daniel J <68441865+djewel23@users.noreply.github.com>
2022-06-13 13:00:55 -0600

    Add event listeners

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const guessFunc = function(){
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess){
+    console.log('Nothing here');
+  };
+};
+
+document.querySelector('.check').addEventListener('click', guessFunc);
+
+// document.querySelector('.check').addEventListener('click', function() {
+//   console.log(document.querySelector('.guess').value);
+// });

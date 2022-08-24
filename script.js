9b5ea4101e5e4d84b3aa748842bac6e4d3d491fe
Daniel J <68441865+djewel23@users.noreply.github.com>
2022-08-24 13:11:26 -0600

    Resets for Again button and high score

--- script.js
+++ script.js
@@ -9,8 +9,9 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const ranNumber = Math.trunc(Math.random() * 20) + 1;
+let ranNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 const guessFunc = function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,8 +24,15 @@ const guessFunc = function () {
     // When player wins
   } else if (guess === ranNumber) {
     document.querySelector('.message').textContent = '🎊 Correct number!';
+    document.querySelector('.number').textContent = ranNumber;
     document.querySelector('body').style.backgroundColor = '#60B347';
     document.querySelector('.number').style.width = '30rem';
+
+    // Update High Score
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // Guess is too high
   } else if (guess > ranNumber) {
@@ -51,7 +59,23 @@ const guessFunc = function () {
 };
 
 document.querySelector('.check').addEventListener('click', guessFunc);
-document.querySelector('.number').textContent = ranNumber;
+// Again Button
+document.querySelector('.again').addEventListener('click', function () {
+  // Reset Score and Secret Number
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  ranNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(ranNumber);
+
+  // Reset fields (Message, boxed number, guessing input)
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  // Change styles back
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
 
 // document.querySelector('.check').addEventListener('click', function() {
 //   console.log(document.querySelector('.guess').value);
